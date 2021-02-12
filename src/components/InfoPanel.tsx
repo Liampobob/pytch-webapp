3a3614d07fa5aeb5cc44bb1a2a06d9dc605bc788
Liampobob <liam@thamlock.com>
2021-02-12 15:42:37 +0000

    added initial debug window still WIP

--- src/components/InfoPanel.tsx
+++ src/components/InfoPanel.tsx
@@ -37,6 +37,89 @@ const Errors = () => {
   return <div className="ErrorsPane">{inner}</div>;
 };
 
+enum DataType {
+  string,
+  number,
+  list
+}
+
+class DebugerItem {
+  type: DataType;
+
+  constructor(type:DataType) {
+    this.type = type;
+  }
+
+  display() {
+    return <p className="variable">  </p>;
+  };
+}
+
+class StringItem extends DebugerItem {
+  value: string;
+
+  constructor(val: string) {
+    super(DataType.string);
+    this.value = val;
+  }
+
+  display() {
+    return <p className="variable"> {this.value} </p>;
+  }
+}
+
+class NumberItem extends DebugerItem {
+  value: number;
+
+  constructor(val: number) {
+    super(DataType.number);
+    this.value = val;
+  }
+
+  display() {
+    return <p className="variable"> {this.value} </p>;
+  }
+}
+
+class ListItem extends DebugerItem {
+  value: any[];
+
+  constructor(val: any[]) {
+    super(DataType.list);
+    this.value = val;
+  }
+
+  display() {
+    return <p className="variable"> {this.value} </p>;
+  }
+}
+
+const Debuger = () => {
+  const variables : DebugerItem[] = [
+    new StringItem("hi"),
+    new NumberItem(0),
+    new ListItem([0,1,2])
+  ];
+  const inner =[
+    <button type="button">Rerun</button>,
+    <button type="button">Play</button>,
+    <button type="button">Pause</button>,
+    <button type="button">Step Forward</button>,
+    <button type="button">Step Out</button>,
+    <button type="button">Step Back</button>,
+    <button type="button">Terminate</button>,
+    <button type="button">Mute Breakpoints</button>,
+    <p className="tmp">
+      Debugger WIP
+    </p>
+  ];
+  let i = 0;
+  variables.forEach((a) => inner.push(<div><p> {i++} </p> {a.display()} </div>))
+
+  return <div className="StandardOutputPane">{inner}</div>;
+};
+
+
 const InfoPanel = () => {
   const isSyncingFromBackEnd = useStoreState(
     (state) => state.activeProject.syncState.loadState === "pending"
@@ -77,6 +160,9 @@ const InfoPanel = () => {
         <Tab className="InfoPane" eventKey="errors" title="Errors">
           <Errors />
         </Tab>
+        <Tab className="InfoPane" eventKey="debug" title="Debug">
+          <Debuger />
+        </Tab>
         {liveReloadEnabled ? (
           <Tab
             className="InfoPane"
@@ -92,3 +178,4 @@ const InfoPanel = () => {
 };
 
 export default InfoPanel;
+
